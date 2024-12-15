ab81f6c9aad6d8f5c89f837dc0a61ec93372f831
Borislav Kavalov <borislav.kavalov@blackswan.com>
2024-12-15 22:10:23 +0200

    enable jpa repositories - state package

--- src/main/java/com/fast/cpat/config/DataSourceConfig.java
+++ src/main/java/com/fast/cpat/config/DataSourceConfig.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "com.fast.cpat.repository")
 class DataSourceConfig {
 
     final SecretsService secretsService;
